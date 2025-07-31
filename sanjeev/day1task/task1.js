c5ebd5ed41f16f80170826bbc6a3e79fbbaa55a3
SANJEEV J <sanjeevjai2006@gmail.com>
2025-07-31 07:33:38 +0530

    Add files via upload

---
+++ sanjeev/day1task/task1.js
@@ -0,0 +1,10 @@
+var name = "Sanjeev";
+let age = 18;
+const country = "India";
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+
+name = "sanjeev";
+age = 19;
---
+++ sanjeev/day4/task10.js
@@ -0,0 +1,13 @@
+// Math Functions
+console.log(Math.random());                  // Random number
+console.log(Math.floor(4.7));               // Rounds down to 4
+console.log(Math.ceil(4.3));                // Rounds up to 5
+console.log(Math.max(3, 5, 1));             // 5
+console.log(Math.min(3, 5, 1));             // 1
+
+// String Functions
+let name = "sanjeev";
+console.log(name.toUpperCase());            // "SANJEEV"
+console.log(name.slice(0, 3));              // "san"
+console.log(name.includes("jeev"));          // true
+console.log(name.replace("san", "shan"));    // "shanjeev"
